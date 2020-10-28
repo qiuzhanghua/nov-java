cb3c8baead8d4896640bc2e3f83232bdb2b2c424
qiuzhanghua <qiuzhanghua@icloud.com>
2020-10-28 09:15:33 +0800

    Enjoy Coding

---
+++ src/main/java/com/example/talent/nov/Problem.java
@@ -0,0 +1,10 @@
+package com.example.talent.nov;
+
+import java.util.List;
+
+public class Problem {
+    public boolean solvable(List<String> words, String result) {
+
+        return false;
+    }
+}
---
+++ src/test/java/com/example/talent/nov/ProblemTests.java
@@ -0,0 +1,70 @@
+package com.example.talent.nov;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProblemTests {
+
+    @DisplayName("0+0=0")
+    @Test
+    public void test_solvable() {
+        Problem problem = new Problem();
+        assertTrue(problem.solvable(List.of("A", "A"), "A"));
+    }
+
+    @DisplayName("X+0=X")
+    @Test
+    public void test_solvable_02() {
+        Problem problem = new Problem();
+        assertTrue(problem.solvable(List.of("A", "B"), "A"));
+    }
+
+    @DisplayName("0+0=00")
+    @Test
+    public void test_solvable_03() {
+        Problem problem = new Problem();
+        assertFalse(problem.solvable(List.of("A", "A"), "AA"));
+    }
+
+    @DisplayName("XXX+00=XXX")
+    @Test
+    public void test_solvable_04() {
+        Problem problem = new Problem();
+        assertFalse(problem.solvable(List.of("AAA", "BB"), "BB"));
+    }
+
+    @DisplayName("SIX + SIX + SEVEN = TWENTY")
+    @Test
+    public void test_solvable_05() {
+        Problem problem = new Problem();
+        assertTrue(problem.solvable(List.of("SIX", "SIX", "SEVEN"), "TWENTY"));
+
+    }
+
+    @DisplayName("I + THINK + IT + BE + THINE = INDEED")
+    @Test
+    public void test_solvable_06() {
+        Problem problem = new Problem();
+        assertTrue(problem.solvable(List.of("I", "THINK", "IT", "BE", "THINE"), "INDEED"));
+    }
+
+    @DisplayName("I + THINK + IT + BE + THINE = INDEED")
+    @Test
+    public void test_solvable_07() {
+        Problem problem = new Problem();
+        assertTrue(problem.solvable(List.of("SEND", "MORE"), "MONEY"));
+    }
+
+
+    @DisplayName("I + YOU = LOVE")
+    @Test
+    public void test_solvable_08() {
+        Problem problem = new Problem();
+        assertFalse(problem.solvable(List.of("I", "YOU"), "LOVE"));
+    }
+
+}
